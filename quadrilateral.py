004e5e0704fff5a312d9b182d77c219d306ca115
Venika Sem <venika.sem@mths.ca>
2022-11-09 16:28:28 +0000

    update

--- quadrilateral.py
+++ quadrilateral.py
@@ -2,48 +2,43 @@
 
 # Created by: Venika Sem
 # Created on: Nov 2022
-# This program tells the month quadrilateral on the inputted side
-
-import math
+# This program tells user what polygon it is based on side
 
 
 def main():
-    # this function tells the month quadrilateral on the inputted side
+    # this function tells user what polygon it is based on side
 
     # input
-    month_number = int(input("Enter a number of sides: "))
+    side_as_string = input("Enter a number of sides: ")
     print("")
 
     # process & output
-    match month_number:
-        case 1:
-            print("January")
-        case 2:
-            print("February")
-        case 3:
-            print("March")
-        case 4:
-            print("April")
-        case 5:
-            print("May")
-        case 6:
-            print("June")
-        case 7:
-            print("July")
-        case 8:
-            print("August")
-        case 9:
-            print("September")
-        case 10:
-            print("October")
-        case 11:
-            print("November")
-        case 12:
-            print("December")
-        case _:
-            print("Invalid input, not a month")
-
-    print("\nDone.")
+    try:
+        side_as_number = int(side_as_string)
+        # process & output
+        match side_as_number:
+            case 3:
+                print("A polygon with {0} sides is a triangle.".format(side_as_number))
+            case 4:
+                print("A polygon with {0} sides is a quadrilateral.".format(side_as_number))
+            case 5:
+                print("A polygon with {0} sides is a pentagon.".format(side_as_number))
+            case 6:
+                print("A polygon with {0} sides is a hexagon.".format(side_as_number))
+            case 7:
+                print("A polygon with {0} sides is a heptagon.".format(side_as_number))
+            case 8:
+                print("A polygon with {0} sides is a octagon.".format(side_as_number))
+            case 9:
+                print("A polygon with {0} sides is a nonagon.".format(side_as_number))
+            case 10:
+                print("A polygon with {0} sides is a decagon.".format(side_as_number))
+            case _:
+                print("Invalid input, sides must ONLY be between 3 and 10!")
+    except ValueError:
+        print("{0} is not a valid input".format(side_as_string))
+    finally:
+        print("Done.")
 
 
 if __name__ == "__main__":
